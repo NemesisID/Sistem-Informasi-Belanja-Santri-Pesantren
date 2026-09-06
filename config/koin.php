5f738a040b4e2fae3612dfd7c5322c4ae9509671
Nrabyan-17 <129036095+Nrabyan-17@users.noreply.github.com>
2026-09-06 11:10:18 +0700

    memperbaiki error dari upload batch data santri & bug pencarian data santri

--- config/koin.php
+++ config/koin.php
@@ -9,5 +9,5 @@ return [
     'min_penarikan' => (int) env('KOIN_MIN_PENARIKAN', 1000),
 
     // Maksimal item per halaman pada semua list
-    'max_per_page' => 100,
+    'max_per_page' => (int) env('KOIN_MAX_PER_PAGE', 2000),
 ];
